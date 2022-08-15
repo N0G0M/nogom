f334f7f2075976cfc0f7804685b16f78dbb2f0c4
N0G0M <98800172+N0G0M@users.noreply.github.com>
2022-08-15 20:09:26 +0545

    Delete om directory

--- om/java.js
+++
@@ -1,9 +0,0 @@
-var audio = new Audio("/sound/click.mp3");
-
-document.onclick = function() {
-  audio.play();
-}
-window.addEventListener('load',()=>{
-  const preload=document.querySelector('.preload');
-  preload.classList.add("complete")
-});
